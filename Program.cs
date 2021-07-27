fb96fe3b32f7b190ad6541d9f53a119596031c26
Harshverma07 <harsh0507verma@gmail.com>
2021-07-27 17:12:13 +0530

    code change

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace devOps
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello and  World!");
         }
     }
 }
