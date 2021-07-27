21ce9559aba80d6e34e79703aca44aaac0151151
Harshverma07 <harsh0507verma@gmail.com>
2021-07-27 17:16:07 +0530

    int

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@ namespace devOps
 {
     class Program
     {
+        int a = 20;
         static void Main(string[] args)
         {
+            Console.WriteLine('a');
             Console.WriteLine("Hello and  World!");
         }
     }
