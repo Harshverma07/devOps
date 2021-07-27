005c0e4d2e5abbda2e8c584b433013f12a0d0062
Harshverma07 <harsh0507verma@gmail.com>
2021-07-27 17:20:09 +0530

    yes

--- Program.cs
+++ Program.cs
@@ -5,10 +5,17 @@ namespace devOps
     class Program
     {
         int a = 20;
+        public  void edge()
+        {
+            int b = 23;
+            string c = "hello";
+        }
         static void Main(string[] args)
         {
             Console.WriteLine('a');
+            Console.WriteLine('b');
+
             Console.WriteLine("Hello and  World!");
-        }
+        } 
     }
 }
