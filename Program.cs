baf62749e4133ea473a2da9975ae52d132afe6a3
Harshverma07 <harsh0507verma@gmail.com>
2021-07-27 17:02:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace devOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
