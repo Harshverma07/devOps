e734e87a787604c92518c4580edd28e2070137c0
Harshverma07 <harsh0507verma@gmail.com>
2021-07-28 09:59:13 +0530

    ok done

--- Program.cs
+++ Program.cs
@@ -5,15 +5,17 @@ namespace devOps
     class Program
     {
         int a = 20;
+        
         public  void edge()
         {
             int b = 23;
             string c = "hello";
-            int g = 34;
+        
         }
         static void Main(string[] args)
         {
             Console.WriteLine('a');
+          
             Console.WriteLine('b');
             Console.WriteLine('g');
 
